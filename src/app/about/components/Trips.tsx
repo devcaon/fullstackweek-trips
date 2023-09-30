768ba3a5d59ca1fbf1a947717b61b9f93e7d9ec0
Claudio Oliveira <dev.caon@gmail.com>
2023-09-30 17:30:24 -0300

    feat: update database schema

--- src/app/about/components/Trips.tsx
+++ src/app/about/components/Trips.tsx
@@ -1,7 +1,21 @@
 
-const Trips = () => {
+import { prisma } from "@/lib/prisma"
+
+const getTrips = async () => {
+  const trips = await prisma.trip.findMany({})
+  return trips
+}
+
+const Trips = async () => {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(res => res.json())
+  
   return (
-    <div>Trips component</div>
+    <div>
+      {data.map((i: any) => (
+        <p key={i.id}>{i.body}</p>
+      ))}
+    </div>
   )
 }
 
