e00883f510e587f17bafc699d382a69c9368ff5e
Claudio Oliveira <dev.caon@gmail.com>
2023-10-11 06:33:54 -0300

    style: add border too trip reservation component

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -10,19 +10,21 @@ interface TripReservationProps {
   trip: Trip
 }
 
-const TripReservation = ({trip} : TripReservationProps) => {
+const TripReservation = ({ trip }: TripReservationProps) => {
   return (
     <div className="flex flex-col px-5">
       <div className="flex gap-3 mb-3">
-        <DatePicker placeholderText='Data inicial' className="w-full" onChange={() => {}}/>
-        <DatePicker placeholderText='Data final' className="w-full" onChange={() => {}}/>
+        <DatePicker placeholderText='Data inicial' className="w-full" onChange={() => { }} />
+        <DatePicker placeholderText='Data final' className="w-full" onChange={() => { }} />
       </div>
       <Input placeholder={`Número de hóspedes (max: ${trip.maxGuests})`} />
       <div className="flex justify-between mt-3">
         <p className='font-medium text-sm text-primaryDark'>Total: </p>
         <p>R$2500</p>
       </div>
-      <Button className='mt-3'>Reservar agora</Button>
+      <div className="pb-10 border-b border-b-primaryGray w-full">
+        <Button className='mt-3 w-full'>Reservar agora</Button>
+      </div>
     </div>
   )
 }
