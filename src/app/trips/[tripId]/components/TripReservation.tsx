8fbb8adec918c5971c1872849af1f400ef668f1a
Claudio Oliveira <dev.caon@gmail.com>
2023-10-11 06:22:56 -0300

    feat: add trip reservation component

---
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import Button from '@/components/Button'
+import DatePicker from '@/components/DatePicker'
+import Input from '@/components/Input'
+import { Trip } from '@prisma/client'
+import React from 'react'
+
+interface TripReservationProps {
+  trip: Trip
+}
+
+const TripReservation = ({trip} : TripReservationProps) => {
+  return (
+    <div className="flex flex-col px-5">
+      <div className="flex gap-3 mb-3">
+        <DatePicker placeholderText='Data inicial' className="w-full" onChange={() => {}}/>
+        <DatePicker placeholderText='Data final' className="w-full" onChange={() => {}}/>
+      </div>
+      <Input placeholder={`Número de hóspedes (max: ${trip.maxGuests})`} />
+      <div className="flex justify-between mt-3">
+        <p className='font-medium text-sm text-primaryDark'>Total: </p>
+        <p>R$2500</p>
+      </div>
+      <Button className='mt-3'>Reservar agora</Button>
+    </div>
+  )
+}
+
+export default TripReservation
